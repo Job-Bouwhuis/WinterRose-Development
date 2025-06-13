4eef80e1fa149ef5aff3a8a6494c20f84aad58ed
Job Bouwhuis <jobbouwhuis@hotmail.com>
2025-06-13 20:16:34 +0200

    started on the AssetDatabase system, but spent my time improving the Anonymous type system in WinterRose.Reflection and WinterForge because it will be used and needed more features

---
+++ WinterRose.FrostWarden/WinterRose.FrostWarden/AssetDatabase/Assets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinterRose.FrostWarden
+{
+    public static class Assets
+    {
+
+    }
+}
